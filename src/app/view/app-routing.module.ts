5a61b50ca39c097010cb02a696ab04d0668048dd
haimMontreal <jhchoukroun@gmail.com>
2019-04-01 21:55:02 -0400

    ng build work

--- src/app/view/app-routing.module.ts
+++ src/app/view/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import {DisplayResultComponent} from './display-result/display-result.component';
 import {EpisodesComponent} from './episodes/episodes.component';
 import {AppComponent} from './app.component';
-import {NotFoundComponent} from '../not-found/not-found.component';
+import {NotFoundComponent} from './not-found/not-found.component';
 import {main} from '@angular/compiler-cli/src/main';
 
 
