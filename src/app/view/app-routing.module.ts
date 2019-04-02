7adc36afd06625efce0548484a0cbe369b467b47
haimMontreal <jhchoukroun@gmail.com>
2019-04-01 21:30:45 -0400

    before doing ng build adding seaons(episodes)

--- src/app/view/app-routing.module.ts
+++ src/app/view/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {DisplayResultComponent} from './display-result/display-result.component';
-
+import {EpisodesComponent} from './episodes/episodes.component';
+import {AppComponent} from './app.component';
+import {NotFoundComponent} from '../not-found/not-found.component';
+import {main} from '@angular/compiler-cli/src/main';
 
 
 const routes: Routes = [
-  {path: 'search/:id', component: DisplayResultComponent}
-  //{path: '**', component: notFound}
+  {path: 'search/:id', component: DisplayResultComponent},
+  {path: 'episodes/:id', component: EpisodesComponent},
 ];
 
 @NgModule({
--- src/app/view/episodes/episodes.component.ts
+++ src/app/view/episodes/episodes.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import {DataService} from '../../api/data.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
-  selector: 'app-episodes',
+  selector: 'app-seasons',
   templateUrl: './episodes.component.html',
   styleUrls: ['./episodes.component.sass']
 })
 export class EpisodesComponent implements OnInit {
 
-  constructor() { }
+  showId: string;
+  seasons: any;
+  constructor(private dataService: DataService,
+              private route: ActivatedRoute) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.route.paramMap.subscribe( (params) => {
+        this.showId = params.get('id');
+        this.getShows();
+      }
+    );
+  }
+
+  getShows(): void {
+    this.dataService.getSeasons(this.showId).subscribe((results) => {
+      this.seasons = results;
+    });
   }
 
 }
